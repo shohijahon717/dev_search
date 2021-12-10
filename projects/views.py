b24491cb142d132f4cdaafff02d98c4c24367f60
shohijahon717 <shohijahonyodgorov2141@gmail.com>
2021-12-10 17:10:00 +0500

    accountda + project crud qo'shildi

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from .models import Project
+from .models import Project, Tag
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -21,18 +21,22 @@ def project(request, pk):
 
 @login_required(login_url='login')
 def createProject(request):
-    form = ProjectForm
+    profile = request.user.profile
+    form = ProjectForm()
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('projects')
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect('account')
     context = {"form": form}
     return render(request, 'projects/project_form.html', context)
 
 @login_required(login_url='login')
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES, instance=project)
@@ -44,11 +48,12 @@ def updateProject(request, pk):
 
 @login_required(login_url='login')
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     context = {'object': project}
     if request.method == 'POST':
         project.delete()
-        return redirect('projects')
+        return redirect('account')
     return render(request, 'projects/delete_template.html', context)
 
 
